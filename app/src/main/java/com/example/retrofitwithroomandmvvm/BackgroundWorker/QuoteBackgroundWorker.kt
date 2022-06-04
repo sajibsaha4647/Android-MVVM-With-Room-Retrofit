cc8fa68c7c3df5113dd089b91acab3a7c8431ea1
sajibsaha4647 <sksaha4648@gmail.com>
2022-06-04 10:45:59 +0600

    updated

--- app/src/main/java/com/example/retrofitwithroomandmvvm/BackgroundWorker/QuoteBackgroundWorker.kt
+++ app/src/main/java/com/example/retrofitwithroomandmvvm/BackgroundWorker/QuoteBackgroundWorker.kt
@@ -1,6 +1,7 @@
 package com.example.retrofitwithroomandmvvm.BackgroundWorker
 
 import android.content.Context
+import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.example.retrofitwithroomandmvvm.MainApplication
@@ -9,11 +10,14 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class QuoteBackgroundWorker(private val context: Context,params:WorkerParameters):Worker {
+class QuoteBackgroundWorker(private val context: Context,params:WorkerParameters): Worker(context,params) {
     override fun doWork(): Result {
+        Log.d("CoroutineScope","CoroutineScope")
         val repositorys = (context as MainApplication).repositorys
         CoroutineScope(Dispatchers.IO).launch {
+            repositorys.getQuotesackground()
 
         }
+        return Result.success()
     }
 }
--- app/src/main/java/com/example/retrofitwithroomandmvvm/MainApplication.kt
+++ app/src/main/java/com/example/retrofitwithroomandmvvm/MainApplication.kt
@@ -1,10 +1,19 @@
 package com.example.retrofitwithroomandmvvm
 
 import android.app.Application
+import android.provider.SyncStateContract
+import android.util.Log
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
 import com.example.retrofitwithroomandmvvm.ApiService.Apiservice
 import com.example.retrofitwithroomandmvvm.ApiService.RetrofitHelper
+import com.example.retrofitwithroomandmvvm.BackgroundWorker.QuoteBackgroundWorker
 import com.example.retrofitwithroomandmvvm.Repository.Repositorys
 import com.example.retrofitwithroomandmvvm.Room.Databse.DatabaseSet
+import java.util.concurrent.TimeUnit
 
 class MainApplication : Application() {
 
@@ -13,6 +22,14 @@ class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         initialized()
+        setupWorker()
+    }
+
+    private fun setupWorker(){
+        val constraint = Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+        val workerRequest = PeriodicWorkRequest.Builder(QuoteBackgroundWorker::class.java,1, TimeUnit.MINUTES).setConstraints(constraint).build()
+        WorkManager.getInstance(this).enqueue(workerRequest)
+        Log.d("CoroutineScope","CoroutineScope")
     }
 
     private fun initialized() {
